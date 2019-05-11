4bf8e296b2b94a97c16047f2d8df665fdf8208da
yangfuen2019 <yfe008@126.com>
2019-05-11 15:52:26 +0800

    1111

--- src/main/java/com/gopher/system/model/CpInSite.java
+++ src/main/java/com/gopher/system/model/CpInSite.java
@@ -18,7 +18,7 @@ public class CpInSite implements Serializable {
     /**
      * 描述
      */
-    private String dsc;
+    private String des;
 
     private String url;
 
@@ -62,15 +62,16 @@ public class CpInSite implements Serializable {
         this.name = name;
     }
 
-    public String getDsc() {
-        return dsc;
-    }
+ 
+    public String getDes() {
+		return des;
+	}
 
-    public void setDsc(String dsc) {
-        this.dsc = dsc;
-    }
+	public void setDes(String des) {
+		this.des = des;
+	}
 
-    public String getUrl() {
+	public String getUrl() {
         return url;
     }
 
@@ -118,62 +119,4 @@ public class CpInSite implements Serializable {
         this.updateTime = updateTime;
     }
 
-    @Override
-    public boolean equals(Object that) {
-        if (this == that) {
-            return true;
-        }
-        if (that == null) {
-            return false;
-        }
-        if (getClass() != that.getClass()) {
-            return false;
-        }
-        CpInSite other = (CpInSite) that;
-        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
-            && (this.getName() == null ? other.getName() == null : this.getName().equals(other.getName()))
-            && (this.getDsc() == null ? other.getDsc() == null : this.getDsc().equals(other.getDsc()))
-            && (this.getUrl() == null ? other.getUrl() == null : this.getUrl().equals(other.getUrl()))
-            && (this.getLanguage() == null ? other.getLanguage() == null : this.getLanguage().equals(other.getLanguage()))
-            && (this.getCreateUser() == null ? other.getCreateUser() == null : this.getCreateUser().equals(other.getCreateUser()))
-            && (this.getUpdateUser() == null ? other.getUpdateUser() == null : this.getUpdateUser().equals(other.getUpdateUser()))
-            && (this.getCreateTime() == null ? other.getCreateTime() == null : this.getCreateTime().equals(other.getCreateTime()))
-            && (this.getUpdateTime() == null ? other.getUpdateTime() == null : this.getUpdateTime().equals(other.getUpdateTime()));
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
-        result = prime * result + ((getName() == null) ? 0 : getName().hashCode());
-        result = prime * result + ((getDsc() == null) ? 0 : getDsc().hashCode());
-        result = prime * result + ((getUrl() == null) ? 0 : getUrl().hashCode());
-        result = prime * result + ((getLanguage() == null) ? 0 : getLanguage().hashCode());
-        result = prime * result + ((getCreateUser() == null) ? 0 : getCreateUser().hashCode());
-        result = prime * result + ((getUpdateUser() == null) ? 0 : getUpdateUser().hashCode());
-        result = prime * result + ((getCreateTime() == null) ? 0 : getCreateTime().hashCode());
-        result = prime * result + ((getUpdateTime() == null) ? 0 : getUpdateTime().hashCode());
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", name=").append(name);
-        sb.append(", dsc=").append(dsc);
-        sb.append(", url=").append(url);
-        sb.append(", language=").append(language);
-        sb.append(", createUser=").append(createUser);
-        sb.append(", updateUser=").append(updateUser);
-        sb.append(", createTime=").append(createTime);
-        sb.append(", updateTime=").append(updateTime);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
-    }
 }
--- src/main/java/com/gopher/system/worker/SynBasicsData.java
+++ src/main/java/com/gopher/system/worker/SynBasicsData.java
@@ -17,9 +17,9 @@ public class SynBasicsData {
 		//同步商家
 		synDataService.synStoreData();
 		//同步优惠卷
-		synDataService.synCouponData();
+		//synDataService.synCouponData();
 		//同步类型
-		synDataService.synTypeData();
+		//synDataService.synTypeData();
 		//同步站点
 		//synDataService.synInSiteData();
 		//同步爬虫
