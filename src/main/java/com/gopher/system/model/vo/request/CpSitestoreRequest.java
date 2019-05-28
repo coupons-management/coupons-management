8cf8c8464f0b765ac4cf107cd9473afac8307c1b
yangfuen2019 <yfe008@126.com>
2019-05-28 09:12:34 +0800

    888888888888

---
+++ src/main/java/com/gopher/system/model/vo/request/CpSitestoreRequest.java
@@ -0,0 +1,14 @@
+package com.gopher.system.model.vo.request;
+
+public class CpSitestoreRequest {
+	private int siteId;
+
+	public int getSiteId() {
+		return siteId;
+	}
+
+	public void setSiteId(int siteId) {
+		this.siteId = siteId;
+	}
+
+}
