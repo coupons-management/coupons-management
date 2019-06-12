b32d9f7e0fae773255855d2ddb2a1cf2e8009ef8
yangfuen2019 <yfe008@126.com>
2019-06-12 23:08:13 +0800

    55555555555

---
+++ src/main/java/com/gopher/system/util/DataCacheUtils.java
@@ -0,0 +1,17 @@
+package com.gopher.system.util;
+
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * 存放数据缓存
+ * @author yfe
+ *
+ */
+public class DataCacheUtils {
+	final public static Integer VALUES=1000;
+	/**
+	 * 优惠卷权值
+	 */
+	public static Map<String,Integer>scrapyMap=new HashMap<String,Integer>();
+
+}
